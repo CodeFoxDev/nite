95dd6a8a412a1fc72226905650cc82e98fd3a7fc
Codefoxdev <admin@codefoxdev.com>
2024-01-11 21:30:22 +0100

    Temporarily disabled cache, because it doesn't really improve speed

--- src/modules/pluginContainer.ts
+++ src/modules/pluginContainer.ts
@@ -137,11 +137,11 @@ export function createPluginContainer(config: ResolvedConfig, moduleGraph: Modul
     },
     async transform(code, id) {
       const mod = moduleGraph.getModulesByFile(id);
-      const cache = mod.getCachedModule();
+      /* const cache = mod.getCachedModule();
       if (cache !== null && typeof cache == "object") {
         const loaded = await cache.loadCache();
         if (typeof loaded == "string") return { code: loaded };
-      }
+      } */
       let _sorted = getSortedPluginsByHook("transform", plugins);
 
       for (plugin of _sorted) {
